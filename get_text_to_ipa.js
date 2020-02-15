7a795697f0e66527253e9d37e4cc77102de2d748
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2020-02-15 10:09:42 -0800

    added a way to get image

--- get_text_to_ipa.js
+++ get_text_to_ipa.js
@@ -1,18 +1,30 @@
 import axios from 'axios';
 // input selected text --> ipa call on English-to-IPA script
 const getSelectedText = (text) => {
-    let englishToIPA_API = "https://localhost:8080/ipa_api";
+    let englishToIPA_API = "https://localhost:8000/ipa_api";
 
     axios.post(englishToIPA_API, {
         text: text
     })
-    .then(response => {
-        console.log(response.data);
-        return response.data;
-    }, error => {
-        console.error(error);
-    })
+        .then(response => {
+            console.log(response.data);
+            return response.data;
+        }, error => {
+            console.error(error);
+        })
 };
 
-return getSelectedText;
+// obtain image url --> output image
+const displayImage = (ipa) => {
+    let IPAImage = "https://localhost:8000/api/{ipa}";
+
+    axios.get(IPAImage)
+        .then(response => {
+            console.log(response);
+        }, error => {
+            console.error(error);
+        });
+};
+
+return {getSelectedText, displayImage};
 
