acba89cfa0aee1dfa454b3456aea1aba0d6a3e0e
Trevor <tdkollin@edu.uwaterloo.ca>
2020-02-15 11:03:36 -0800

    testing images on frontend

--- backend/IPA_Plugin/IPA_Plugin/settings.py
+++ backend/IPA_Plugin/IPA_Plugin/settings.py
@@ -39,10 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'token_to_picture',
     'rest_framework',
-    #'corsheaders',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
