2aef416d20b66a8213c1c5011ef19a76a23862e5
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2020-02-15 23:00:04 -0800

    combine plugin with front end

---
+++ frontend/ipa_plugin/src/js/contentScript.js
@@ -0,0 +1,14 @@
+var height = '60px';
+var iframe_base = document.createElement('iframe');
+iframe_base.src = chrome.extension.getURL('../html/toolbar.html');
+iframe_base.style.height = height;
+iframe_base.style.width = '65%';
+iframe_base.style.position = 'fixed';
+iframe_base.style.bottom = '0';
+iframe_base.style.left = '0';
+iframe_base.style.zIndex = '938089'; // Some high value
+document.documentElement.appendChild(iframe_base);
+
+var bodyStyle = document.body.style;
+var cssTransform = 'transform' in bodyStyle ? 'transform' : 'webkitTransform';
+bodyStyle[cssTransform] = 'translateY(' + height + ')';
