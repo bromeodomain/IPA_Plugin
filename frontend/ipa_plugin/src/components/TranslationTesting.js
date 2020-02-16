b4e73037c4d7c13a16e0b527e0776e89010c9cce
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2020-02-15 17:33:38 -0800

    get highlighted text and translate

--- frontend/ipa_plugin/src/components/TranslationTesting.js
+++ frontend/ipa_plugin/src/components/TranslationTesting.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 import TranslatedLetter from "./TranslatedLetter";
 import '../letter.css';
 
-
 const splitText = (word) => {
     return word.split('');
 };
+
+let original_text = "";
 
 class TranslationTesting extends React.Component {
     constructor() {
@@ -14,12 +15,31 @@ class TranslationTesting extends React.Component {
 
         this.state = {
             translationContent: ''
-        }
+        };
+        this.handleClick = this.handleClick.bind(this);
+        this.getHighlightedText = this.getHighlightedText.bind(this);
     }
+
+    handleClick = () => {
+        this.getHighlightedText();
+    };
+
+    getHighlightedText = () => {
+        let text = "";
+
+        if (window.getSelection) {
+            text = window.getSelection().toString();
+        } else if (document.selection && document.selection.type == "Text") {
+            text = document.selection.createRange().text;
+        }
+        console.log(text);
+        original_text = text;
+        console.log(original_text);
+    };
 
     componentDidMount() {
         axios.put('http://localhost:8000/translate/2/', {
-            original_text: ""
+            original_text: original_text
         }).then(axios.get(
             'http://localhost:8000/translate/2/')
             .then(res => {
@@ -40,6 +60,7 @@ class TranslationTesting extends React.Component {
                     return <span className={character} aria-hidden="true" key={index}>{character}</span>;
                 }
             )}
+                <button onClick={this.handleClick}>Translate</button>
             </span>
         );
     }
