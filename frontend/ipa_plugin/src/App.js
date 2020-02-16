3a8eafee34945e28ebae15f2706cfeab4ca7a566
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2020-02-16 09:29:31 -0800

    new and speedy frontend reacc

--- frontend/ipa_plugin/src/App.js
+++ frontend/ipa_plugin/src/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Test from "./components/test.js";
-import TranslationTesting from "./components/TranslationTesting.js"
 
 function App() {
   return (
     <div className="App">
-      <TranslationTesting />
     </div>
   );
 }
